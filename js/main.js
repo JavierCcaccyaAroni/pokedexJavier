5ab2ab215896d677e28ae86626ca5e749d4aa151
JavierCcaccyaAroni <141256497+JavierCcaccyaAroni@users.noreply.github.com>
2023-10-05 08:38:39 -0500

    reforzando API's

--- js/main.js
+++ js/main.js
@@ -3,67 +3,62 @@ const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
 for (let i = 1; i <= 151; i++) {
-    fetch(URL + i)
-        .then((response) => response.json())
-        .then(data => mostrarPokemon(data))
-}
+  async function mostrarPok() {
+    const response = await fetch(URL + i)
+    const data = await response.json()
 
-function mostrarPokemon(poke) {
-
-    let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
+    let tipos = data.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
     tipos = tipos.join('');
 
-    let pokeId = poke.id.toString();
+    let pokeId = data.id.toString();
     if (pokeId.length === 1) {
-        pokeId = "00" + pokeId;
+      pokeId = "00" + pokeId;
     } else if (pokeId.length === 2) {
-        pokeId = "0" + pokeId;
+      pokeId = "0" + pokeId;
     }
-
 
     const div = document.createElement("div");
     div.classList.add("pokemon");
     div.innerHTML = `
         <p class="pokemon-id-back">#${pokeId}</p>
         <div class="pokemon-imagen">
-            <img src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}">
+            <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}">
         </div>
         <div class="pokemon-info">
             <div class="nombre-contenedor">
                 <p class="pokemon-id">#${pokeId}</p>
-                <h2 class="pokemon-nombre">${poke.name}</h2>
+                <h2 class="pokemon-nombre">${data.name}</h2>
             </div>
             <div class="pokemon-tipos">
                 ${tipos}
             </div>
             <div class="pokemon-stats">
-                <p class="stat">${poke.height}m</p>
-                <p class="stat">${poke.weight}kg</p>
+                <p class="stat">${data.height}m</p>
+                <p class="stat">${data.weight}kg</p>
             </div>
         </div>
     `;
     listaPokemon.append(div);
-}
+  }
+  mostrarPok()
 
-botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
+  botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
     const botonId = event.currentTarget.id;
-
     listaPokemon.innerHTML = "";
+  
+    if (botonId === "ver-todos") {
+      mostrarPok();
+    } else {
 
-    for (let i = 1; i <= 151; i++) {
-        fetch(URL + i)
-            .then((response) => response.json())
-            .then(data => {
-
-                if(botonId === "ver-todos") {
-                    mostrarPokemon(data);
-                } else {
-                    const tipos = data.types.map(type => type.type.name);
-                    if (tipos.some(tipo => tipo.includes(botonId))) {
-                        mostrarPokemon(data);
-                    }
-                }
-
-            })
+      async function listCateg() {
+        const response = await fetch(URL + i)
+        const data = await response.json()
+        const tipos = data.types.map(type => type.type.name);
+        if (tipos.some(tipo => tipo.includes(botonId))) {
+          mostrarPok();
+        }
+      }
+      listCateg()
     }
-}))
+  }))
+}
